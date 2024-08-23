b9cc3e160ddd1020bd038af2169880f402bb82e5
navernoKERIL <navernokeril@gmail.com>
2024-08-23 23:51:52 +0300

    d4

--- kvadratkav3.cpp
+++ kvadratkav3.cpp
@@ -2,16 +2,18 @@
 #include <math.h>
 #include <assert.h>
 #include <ctype.h>
-#define TESTS 0
 
-#define CHECK_ERROR(error)\
-{                         \
-if (error != OK)          \
-{                         \
-    check_errors(error);  \
-    return error;         \
-}                         \
-}                         \
+#define TESTS 1
+
+#define CHECK_ERROR(error)    \
+do                            \
+{                             \
+    if (error != OK)          \
+    {                         \
+        check_errors(error);  \
+        return error;         \
+    }                         \
+}while (0)                    \
 
 struct Data
 {
@@ -23,10 +25,9 @@ struct Data
     double x2;
 
     int roots;
-    int n;
 };
 
-enum Num_of_roots{
+enum NumOfRoots{
     NO_ROOTS = 0,
     ONE_ROOT,
     TWO_ROOTS,
@@ -36,32 +37,36 @@ enum Num_of_roots{
 enum Compare{
     LESS = -1,
     EQUAL,
-    GREATER
+    GREATER,
+    NaN
 };
 
-enum Error_codes{
+enum ErrorCodes{
     OK = 0,
     COEF_INPUT,
-    PRINT_ERR
+    PRINT_ERR,
+    BUFF_ERR,
+    NAN_X,
+    NO_COEF
 };
 
 const double EPS = 1e-9;
 
-Error_codes get_square_root(Data *);
-Error_codes get_lin_root(Data *, double, double);
-Error_codes get_coefs(Data *);
+ErrorCodes get_square_root(Data *);
+ErrorCodes get_lin_root(Data *);
+ErrorCodes get_coefs(Data *);
+ErrorCodes print_roots(Data *);
+ErrorCodes buff_clear(void);
+ErrorCodes get_coef(double*, char*);
+ErrorCodes symbol_verification();
 Compare compare_doubles(double, double);
 void check_errors(int);
-void print_roots(Data *);
-void get_coef(double*, char*);
-void buff_clear(void);
 void main_testings();
-int  one_test(Data *);
-int  symbol_verification(int);
+int  one_test(int, Data *);
 void print_equation(Data *);
 bool compare_pairs(double, double, double, double);
 void struct_initialize(Data *);
-void test_err(Data *, double, double);
+void test_err(int, Data *, double, double);
 
 
 
@@ -72,80 +77,111 @@ int main(void)
 #if TESTS
     main_testings();
 #else
-    Data coefs = {0, 0, 0, 0, 0, 0, 0};
+    Data coefs = {};
     struct_initialize(&coefs);
-    int error = get_coefs(&coefs);
+
+    int error = OK;
+
+    error = get_coefs(&coefs);
     CHECK_ERROR(error);
 
     error = get_square_root(&coefs);
     CHECK_ERROR(error);
 
-    print_roots(&coefs);
+    error = print_roots(&coefs);
     CHECK_ERROR(error);
 #endif
-    return 0;
+    return OK;
 }
 
 
+//-------------------------------------------------------------------------------
 
 
-
-Error_codes get_coefs(Data *coefs)
+ErrorCodes get_coefs(Data *coefs)
 {
     assert(coefs != nullptr);
 
-    puts("ax^2 + bx + c = 0");
-    get_coef(&coefs->a, "a");
-    get_coef(&coefs->b, "b");
-    get_coef(&coefs->c, "c");
+    double a = NAN, b = NAN, c = NAN;
 
-    if (coefs->a == NAN || coefs->b == NAN || coefs->c == NAN){
-        check_errors(COEF_INPUT);
+    puts("ax^2 + bx + c = 0");
+
+    ErrorCodes err = get_coef(&a, "a");
+    if (err)
+        return err;
+
+    err = get_coef(&b, "b");
+    if (err)
+        return err;
+
+    err = get_coef(&c, "c");
+    if (err)
+        return err;
+
+    if (isnan(a) || isnan(b) || isnan(c))
         return COEF_INPUT;
-    }
+
+    coefs->a = a;
+    coefs->b = b;
+    coefs->c = c;
 
     print_equation(coefs);
     return OK;
 }
 
-void get_coef(double *coef, char *str)
+ErrorCodes get_coef(double *coef, char *str)
 {
-    assert(coef != nullptr && str != nullptr);
+    assert(coef != nullptr);
+    assert(str  != nullptr);
 
-    for(int i = 0; i < 1; i++){
+    int i = 0;
+    for(; i < 3; i++){
         printf("write coefficient %s ", str);
 
         scanf("%lg", coef);
 
-        i = symbol_verification(i);
+        int result = symbol_verification();
+        if (result == BUFF_ERR)
+            return BUFF_ERR;
+        if (result == OK)
+            break;
     }
+    if (i == 3)
+        return NO_COEF;
+    return OK;
 }
 
-int symbol_verification(int i)
+ErrorCodes symbol_verification()
 {
     char ch  = getchar();
     if (ch!='\n' && ch != EOF){
-        if (isspace(ch) == 0){
+        if (isspace(ch) == 0)
             printf("wrong symbol (%c)\n", ch);
-            i--;
-        }
-        buff_clear();
+
+        if (buff_clear() == BUFF_ERR)
+            return BUFF_ERR;
+        return NO_COEF;
     }
-    return i;
+    return OK;
 }
 
-inline void buff_clear(void)
+ErrorCodes buff_clear(void)
 {
-    char ch = 0;
-    while(ch!='\n' && ch!=EOF)
+    int ch       = 0;
+    int symb_amt = 0;
+    while(ch != '\n' && ch != EOF){
         ch = getchar();
+        if (symb_amt++ > 1000)
+            return BUFF_ERR;
+        }
+    return OK;
 }
 
 
+//-------------------------------------------------------------------------------
 
 
-
-Error_codes get_square_root(Data *coefs)
+ErrorCodes get_square_root(Data *coefs)
 {
     assert(coefs != nullptr);
 
@@ -154,7 +190,7 @@ Error_codes get_square_root(Data *coefs)
     double c = coefs->c;
 
     if (compare_doubles(a, 0) == EQUAL)
-        get_lin_root(coefs, b, c);
+        get_lin_root(coefs);
 
     else{
         double d = b*b - 4*a*c;
@@ -165,61 +201,68 @@ Error_codes get_square_root(Data *coefs)
         }
 
         coefs->x1 = (-b + sqrt(d))/(2*a);
-        coefs->x2 = (-b - sqrt(d))/(2*a);
 
         if (compare_doubles(d, 0) == EQUAL){
             coefs->roots = ONE_ROOT;
             return OK;
             }
 
+        coefs->x2 = (-b - sqrt(d))/(2*a);
+
         coefs->roots = TWO_ROOTS;
         return OK;
     }
 }
 
-Error_codes get_lin_root(Data *coefs, double b, double c)
+ErrorCodes get_lin_root(Data *coefs)
 {
     assert(coefs != nullptr);
 
+    double b = coefs->b;
+    double c = coefs->c;
+
     #if ! TESTS
     puts("not a quadratic equation");
-
     #endif
 
-    if(compare_doubles(coefs->b, 0) == EQUAL){
-        coefs->roots = (compare_doubles(coefs->c, 0) == EQUAL) ? ANY_ROOT : NO_ROOTS;
+    if(compare_doubles(b, 0) == EQUAL){
+        coefs->roots = (compare_doubles(c, 0) == EQUAL) ? ANY_ROOT : NO_ROOTS;
+
         return OK;
     }
 
-    coefs->x1 = -c/b;
-    coefs->x2 = -c/b;
-
+    coefs->x1    = -c/b;
     coefs->roots = ONE_ROOT;
+
     return OK;
 }
 
 
 
-
+//-------------------------------------------------------------------------------
 
 
 void print_equation(Data *coefs)
 {
     assert(coefs != nullptr);
 
-    int i = 0;
+    int coefs_amt = 0;
     if (compare_doubles(coefs->a, 0) != EQUAL)
-        i += printf("%lgx^2", coefs->a);
+        coefs_amt += printf("%lgx^2", coefs->a);
+
     if (compare_doubles(coefs->b, 0) != EQUAL)
-        i += printf("%+lgx",  coefs->b);
+        coefs_amt += printf("%+lgx",  coefs->b);
+
     if (compare_doubles(coefs->c, 0) != EQUAL)
-        i += printf("%+lg", coefs->c);
-    if (i==0)
+        coefs_amt += printf("%+lg",   coefs->c);
+
+    if (coefs_amt==0)
         printf("0");
+
     puts(" = 0");
 }
 
-void print_roots(Data *coefs)
+ErrorCodes print_roots(Data *coefs)
 {
     assert(coefs != nullptr);
 
@@ -228,25 +271,33 @@ void print_roots(Data *coefs)
             printf("no roots");
             break;
         case ONE_ROOT:
+            if (isnan(coefs->x1))
+                return NAN_X;
             printf("x = %lg", coefs->x1);
             break;
         case TWO_ROOTS:
+            if (isnan(coefs->x1) || isnan(coefs->x2))
+                return NAN_X;
             printf("x1 = %lg, X2 = %lg", coefs->x1, coefs->x2);
             break;
         case ANY_ROOT:
             printf("any root");
             break;
         default:
-            check_errors(PRINT_ERR);
+            return PRINT_ERR;
     }
+    return OK;
 }
 
 
-
+//-------------------------------------------------------------------------------
 
 
 Compare compare_doubles(double a, double b)
 {
+    if (isnan(a))
+        return (isnan (b)) ? EQUAL : NaN;
+
     double dif = a - b;
 
     if (fabs(dif) < EPS)
@@ -273,7 +324,6 @@ void struct_initialize(Data *coefs)
     coefs->c     = NAN;
     coefs->x1    = NAN;
     coefs->x2    = NAN;
-    coefs->n     = NAN;
     coefs->roots = NAN;
 }
 
@@ -282,76 +332,76 @@ void check_errors(int err_code)
     switch(err_code)
     {
         case COEF_INPUT:
-            printf("error with input coefficients");
+            fprintf(stderr, "error with input coefficients");
+            break;
         case PRINT_ERR:
-            printf("error with print answer");
+            fprintf(stderr, "error with print answer");
+            break;
+        case BUFF_ERR:
+            fprintf(stderr, "buff error or to much symbols");
+            break;
+        case NAN_X:
+            fprintf(stderr, "x is NaN");
+            break;
+        case NO_COEF:
+            fprintf(stderr, "to much attempts");
+            break;
         default:
-            printf("? error");
-
+            fprintf(stderr, "? error");
     }
 }
 
 
 
+ //-------------------------------------------------------------------------------
 
 
-#if TESTS
-int one_test(Data *coefs)
+int one_test(int n_test, Data *coefs)
 {
     assert(coefs != nullptr);
 
     double exp1 = coefs->x1, exp2 = coefs->x2;
-    int exproots = coefs->roots;
+    coefs->x1   = NAN;
+    coefs->x2   = NAN;
 
-    if (compare_doubles(coefs->roots, exproots) != EQUAL ||
-       compare_pairs(coefs->x1, coefs->x2, exp1, exp2) == false)
+    int exproots = coefs->roots;
+    coefs->roots = NAN;
+
+    get_square_root(coefs);
+
+    if (coefs->roots != exproots || compare_pairs(coefs->x1, coefs->x2, exp1, exp2) == false)
     {
 
-        test_err(coefs, exp1, exp2);
+        test_err(n_test, coefs, exp1, exp2);
 
-        return coefs->n;
+        return n_test;
     }
     return OK;
 }
 
 void main_testings()
 {
-    int err = 0;
-    Data test1 = {1,   2,  -3,
-                  1,   -3,
-                  1,   2};
-    Data test2 = {0,   0,   0,
-                  0,   0,
-                  2,   3};
-    Data test3 = {1.1, 0,   0,
-                  0,   0,
-                  3,   1};
-    Data test4 = {0,   2.5, 0,
-                  0,   0,
-                  4,   1};
-    Data test5 = {1.1, 1.2, 7.5,
-                  0,   0,
-                  5,   0};
+    int     err = 0;
+    const int test_amt = 5;
+//                           a    b    c    x1    x2  roots
+    Data tests[test_amt] = {{1,   2,  -3,   1,   -3,    2},
+                            {0,   0,   0,   NAN,  NAN,  3},
+                            {1.1, 0,   0,   0,    NAN,  1},
+                            {0,   2.5, 0,   0,    NAN,  1},
+                            {1.1, 1.2, 7.5, NAN,  NAN,  0}};
 
-    err = one_test(&test1);
-    if (err == 0)
-        err = one_test(&test2);
-    if (err == 0)
-        err = one_test(&test3);
-    if (err == 0)
-        err = one_test(&test4);
-    if (err == 0)
-        err = one_test(&test5);
-    if (err == 0)
-        printf("no errors");
+
+    for (int i = 0; i < test_amt; i++)
+        err += one_test((i+1), &tests[i]);
+
+    if (err == OK)
+        printf("all tests completed");
 }
 
-inline void test_err(Data *coefs, double exp1, double exp2)
+inline void test_err(int n_test, Data *coefs, double exp1, double exp2)
 {
     assert(coefs != nullptr);
 
     printf("test %d failed, roots = %d, x1, x2: %lg, %lg, expected: %lg, %lg\n",
-         coefs->n, coefs->roots, coefs->x1, coefs->x2, exp1, exp2);
+         n_test, coefs->roots, coefs->x1, coefs->x2, exp1, exp2);
 }
-
-#endif
