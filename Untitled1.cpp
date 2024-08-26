5d88e951f484094062405ccb7582e6a4091b5c08
navernoKERIL <navernokeril@gmail.com>
2024-08-26 14:58:21 +0300

    .

---
+++ Untitled1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "TXLib.h"
+
+int main(void)
+{
+    txSetConsoleAttr (FOREGROUND_RED | BACKGROUND_BLUE);
+    printf ("А в небе 0x1 есть город 0xE");
+    printf ("А в небе 0x1 есть город 0xE");
+}
